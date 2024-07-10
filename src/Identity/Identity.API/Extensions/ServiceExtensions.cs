64fa82a6f584a974e46240efd9f96c8177421306
huytq@ics-p.vn <huytq@ics-p.vn>
2024-07-10 16:28:25 +0700

    config identity

---
+++ src/Identity/Identity.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Identity.API.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection ConfigDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            return services;
+        }
+    }
+}
--- src/Identity/Identity.API/Program.cs
+++ src/Identity/Identity.API/Program.cs
@@ -1,8 +1,37 @@
+using Identity.API.Database;
+using Identity.API.Models;
+using Identity.API.Services;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer(i =>
+{
+    i.IssuerUri = "https://tedu.com.vn";
+    i.Authentication.CookieLifetime = TimeSpan.FromHours(2);
+})
+    .AddDeveloperSigningCredential()
+    .AddAspNetIdentity<ApplicationUser>()
+    .AddConfigurationStore(options =>
+{
+    options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString);
+})
+    .AddOperationalStore(options => options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString))
+    .Services.AddTransient<IProfileService, ProfileService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,9 +46,15 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+
+app.UseRouting();
+
+app.UseIdentityServer();
 
 app.UseAuthorization();
 
-app.MapControllers();
-
-app.Run();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
