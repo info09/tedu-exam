067633ef37c10d5b742846dfe60eb12ddaa00d48
huytq@ics-p.vn <huytq@ics-p.vn>
2024-07-11 11:00:36 +0700

    Create solution structure

--- src/Identity/Identity.API/Program.cs
+++ src/Identity/Identity.API/Program.cs
@@ -1,60 +1,26 @@
-using Identity.API.Database;
-using Identity.API.Models;
-using Identity.API.Services;
-using IdentityServer4.Services;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddControllers();
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
-
-builder.Services.AddIdentityServer(i =>
+namespace Identity.API
 {
-    i.IssuerUri = "https://tedu.com.vn";
-    i.Authentication.CookieLifetime = TimeSpan.FromHours(2);
-})
-    .AddDeveloperSigningCredential()
-    .AddAspNetIdentity<ApplicationUser>()
-    .AddConfigurationStore(options =>
-{
-    options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString);
-})
-    .AddOperationalStore(options => options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString))
-    .Services.AddTransient<IProfileService, ProfileService>();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseAuthentication();
-
-app.UseRouting();
-
-app.UseIdentityServer();
-
-app.UseAuthorization();
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
