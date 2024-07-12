94cb2a82daa9e7eb705b0104362194ce70dab13e
huytq@ics-p.vn <huytq@ics-p.vn>
2024-07-12 08:45:22 +0700

    CQRS and Application

---
+++ src/BuildingBlock/Examination.Dtos/ExamDto.cs
@@ -0,0 +1,22 @@
+﻿using Examination.Dtos.Enums;
+using System;
+
+namespace Examination.Dtos
+{
+    public class ExamDto
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ShortDes { get; set; }
+
+        public int NumberOfQuestion { get; set; }
+
+        public TimeSpan? Duration { get; set; }
+
+        public Level Level { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
